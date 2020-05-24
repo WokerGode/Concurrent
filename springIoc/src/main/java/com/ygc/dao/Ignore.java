b7411c90bb58efb5d26e76810c8fb61137802eeb
woker <1257185622@qq.com>
2020-05-25 00:37:26 +0800

    gitignore

---
+++ springIoc/src/main/java/com/ygc/dao/Ignore.java
@@ -0,0 +1,11 @@
+package com.ygc.dao;
+
+/**
+ * @Author: yanguochen
+ * @Description:
+ * @Date: Created in 0:31 2020/5/25
+ * @Modified By:
+ */
+public class Ignore {
+    private String string;
+}
